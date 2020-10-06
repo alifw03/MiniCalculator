4270c6e10c8892880797b6e2e0b2f5dcbd341a50
Ali Raza <ali.raza.fw03@gmail.com>
2020-10-06 18:30:01 +0500

    Code improvements

--- src/ios/MiniCalculator.m
+++ src/ios/MiniCalculator.m
@@ -7,10 +7,6 @@
 }
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
-- (void)add:(CDVInvokedUrlCommand*)command;
-- (void)substract:(CDVInvokedUrlCommand*)command;
-- (void)multiply:(CDVInvokedUrlCommand*)command;
-- (void)divide:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation MiniCalculator
@@ -23,74 +19,6 @@
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
     } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)add:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-    if(param1 >= 0 && param2 >= 0) {
-        NSString* total = @(param1 + param2);
-        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:total];
-    }
-    else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)substract:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-    if(param1 >= 0 && param2 >= 0) {
-        NSString* total = @(param1 - param2);
-        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:total];
-    }
-    else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)multiply:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-    if(param1 >= 0 && param2 >= 0) {
-        NSString* total = @(param1 * param2);
-        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:total];
-    }
-    else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)divide:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
-    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
-
-    if(param1 >= 0 && param2 >= 0) {
-        NSString* total = @(param1 / param2);
-        pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:total];
-    }
-    else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
--- www/MiniCalculator.js
+++ www/MiniCalculator.js
@@ -3,21 +3,3 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'MiniCalculator', 'coolMethod', [arg0]);
 };
-
-module.exports.add = function(arg0, success, error) {
-    console.log(error);
-    console.log(arg0);
-    exec(success, error, 'MiniCalculator', 'add', [arg0]);
-};
-
-module.exports.substract = function(arg0, success, error) {
-    exec(success, error, 'MiniCalculator', 'substract', [arg0]);
-};
-
-module.exports.multiply = function(arg0, success, error) {
-    exec(success, error, 'MiniCalculator', 'multiply', [arg0]);
-};
-
-module.exports.divide = function(arg0, success, error) {
-    exec(success, error, 'MiniCalculator', 'divide', [arg0]);
-};
